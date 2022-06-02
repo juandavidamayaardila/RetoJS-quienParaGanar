32ceacb0aede758287e695f27606813a40b5e376
juandavidamayaardila <juandavidamayaardila@gmail.com>
2022-06-01 20:39:37 -0500

    firts commit

---
+++ js/app.js
@@ -0,0 +1,227 @@
+import { data } from "../source/preguntas.js";
+
+
+const container = document.querySelector("#container");
+const txtSuperTitle  = document.createElement("h1");
+txtSuperTitle.textContent = "QUIEN QUIERE GANAR!!";
+
+container.append(txtSuperTitle);
+const divRow  = document.createElement("div");
+divRow.classList.add("row");
+
+const divPuntuacion  = document.createElement("div");
+divPuntuacion.classList.add("col-3",'border','divCenter');
+
+
+const divPregunta  = document.createElement("div");
+divPregunta.classList.add('col-9','border');
+
+const divMainPregunta  = document.createElement("div");
+const divPreguntaBody  = document.createElement("div");
+const txtPregunta  = document.createElement("h1");
+
+
+divPreguntaBody.append(txtPregunta);
+divPreguntaBody.classList.add('divCenter');
+divMainPregunta.append(divPreguntaBody);
+divPregunta.append(divMainPregunta);
+divRow.append(divPregunta);
+
+
+const divMainRespuestas  = document.createElement("div");
+const divMainRespuestaTmp  = document.createElement("div");
+divMainRespuestaTmp.classList.add('row','divRespuesta') //falta agregar el estilo preguntas_1 pregutnar stevent
+
+
+const divRespuesta1  = document.createElement("div");
+divRespuesta1.classList.add('col-6')
+
+const divRespuesta2  = document.createElement("div");
+divRespuesta2.classList.add('col-6')
+
+//segundo renglon de respuetas
+const divMainRespuestaTmp2  = document.createElement("div");
+divMainRespuestaTmp2.classList.add('row','divRespuesta') //falta agregar el estilo preguntas_1 pregutnar stevent
+
+const divRespuesta2a  = document.createElement("div");
+divRespuesta2a.classList.add("col-6")
+
+const divRespuesta2b  = document.createElement("div");
+divRespuesta2b.classList.add("col-6")
+
+
+const btnOpcionA  = document.createElement("button");
+const btnOpcionB  = document.createElement("button");
+const btnOpcionC  = document.createElement("button");
+const btnOpcionD  = document.createElement("button");
+
+btnOpcionA.classList.add('btn','btn-outline-primary','btn-lg','btnPropia');
+btnOpcionB.classList.add('btn','btn-outline-success','btn-lg','btnPropia');
+btnOpcionC.classList.add('btn','btn-outline-info','btn-lg','btnPropia');
+btnOpcionD.classList.add('btn','btn-outline-warning','btn-lg','btnPropia');
+
+
+
+divRespuesta2a.append( btnOpcionC);
+divRespuesta2b.append( btnOpcionD);
+divRespuesta1.append(btnOpcionA);
+divRespuesta2.append(btnOpcionB);
+
+divMainRespuestaTmp2.append(divRespuesta2a,divRespuesta2b);
+divMainRespuestaTmp.append(divRespuesta1,divRespuesta2);
+
+divMainRespuestas.append(divMainRespuestaTmp, divMainRespuestaTmp2);
+divPregunta.append(divMainRespuestas)
+
+
+container.appendChild(divRow);
+
+cargarPregunta();
+
+/**
+ * Creamos el html para cargar la puntuacion
+ */
+ const divMainPuntaje  = document.createElement("div");
+
+ const divMainPuntajeHead  = document.createElement("div");
+ const pPuntaje  = document.createElement("p");
+ const iUsuario  = document.createElement("i");
+ const SpanUser  = document.createElement("span");
+
+ iUsuario.classList.add('fa','fa-user-circle-o');
+ SpanUser.classList.add('puntuacion');
+ pPuntaje.classList.add('puntuacion');
+ 
+ SpanUser.textContent = localStorage.getItem('user');
+
+ pPuntaje.textContent = "Su puntaje actual es: " + 0;
+ localStorage.setItem('puntajeTmp',0);
+
+ const btnFinalizar  = document.createElement("button");
+ const btnReiniciar  = document.createElement("button");
+ btnFinalizar.classList.add('btn','btn-outline-danger');
+ btnReiniciar.classList.add('btn','btn-outline-warning','btnReiniciar');
+ btnFinalizar.textContent = 'Finalizar Juego';
+ btnReiniciar.textContent = 'Reiniciar Juego';
+ 
+ divMainPuntajeHead.append(iUsuario);
+ divMainPuntajeHead.append(SpanUser);
+ divMainPuntajeHead.append(pPuntaje);
+ divMainPuntajeHead.append(btnReiniciar);
+ divMainPuntajeHead.append(btnFinalizar);
+ divMainPuntaje.append(divMainPuntajeHead);
+ divPuntuacion.append(divMainPuntaje);
+ divRow.append(divPuntuacion);
+ 
+
+
+function cargarPregunta(){
+  
+    const index = Math.floor(Math.random()* (data.length-1 - 0 + 1));
+    localStorage.setItem('indexPregunta', index);
+    
+         txtPregunta.textContent = data[index].pregunta;
+         btnOpcionA.textContent = data[index].respuestaFalse2;
+         btnOpcionB.textContent = data[index].respuestaFalse1;
+         btnOpcionC.textContent = data[index].respuestaTrue;
+         btnOpcionD.textContent = data[index].respuestaFalse3;
+ 
+}
+
+btnOpcionA.addEventListener('click',  (e) =>{
+    if(validateAnswer(localStorage.getItem('indexPregunta'), btnOpcionA.textContent)){
+        alert('Respuesta correcta!!!!');
+        sumPuntaje();
+        if(validateWin()){
+            alert('Felicidades has ganado!!!');
+            finalizarJuego();
+        }
+    }else{
+        alert('Respuesta Incorrecta!!!!');
+        finalizarJuego();
+    }
+
+});
+
+btnOpcionB.addEventListener('click',  (e) =>{
+    if(validateAnswer(localStorage.getItem('indexPregunta'), btnOpcionB.textContent)){
+        alert('Respuesta correcta!!!!');
+        sumPuntaje();
+        if(validateWin()){
+            alert('Felicidades has ganado!!!');
+            finalizarJuego();
+        }
+    }else{
+        alert('Respuesta Incorrecta!!!!');
+        finalizarJuego();
+    }
+
+});
+
+btnOpcionC.addEventListener('click',  (e) =>{
+    if(validateAnswer(localStorage.getItem('indexPregunta'), btnOpcionC.textContent)){
+        alert('Respuesta correcta!!!!');
+        sumPuntaje();
+        if(validateWin()){
+            alert('Felicidades has ganado!!!');
+            finalizarJuego();
+        }
+    }else{
+        alert('Respuesta Incorrecta!!!!');
+        finalizarJuego();
+    }
+      
+});
+
+btnOpcionD.addEventListener('click',  (e) =>{
+   
+    if(validateAnswer(localStorage.getItem('indexPregunta'), btnOpcionD.textContent)){
+        alert('Respuesta correcta!!!!');
+        sumPuntaje();
+        if(validateWin()){
+            alert('Felicidades has ganado!!!');
+            finalizarJuego();
+        }
+    }else{
+        alert('Respuesta Incorrecta!!!!');
+        finalizarJuego();
+    }
+   
+});
+
+btnFinalizar.addEventListener('click',  (e) =>{
+    localStorage.setItem('user', '');
+    window.location.href = "index.html";
+   
+});
+
+btnReiniciar.addEventListener('click',  (e) =>{
+    
+    window.location.reload();
+   
+});
+
+const validateAnswer = (indexPregunta, answer) =>{
+    return data[indexPregunta].respuestaTrue === answer;
+}
+
+const sumPuntaje = () =>{
+    localStorage.setItem('puntajeTmp', parseInt(localStorage.getItem('puntajeTmp')) + 10);
+    pPuntaje.textContent = "Su puntuación es: " + localStorage.getItem('puntajeTmp');
+    
+    cargarPregunta();
+
+}
+
+
+const finalizarJuego = () =>{
+    localStorage.setItem('puntajeTmp',0);
+    pPuntaje.textContent = "Su puntuación es: " + localStorage.getItem('puntajeTmp');
+}
+
+
+const validateWin = () =>{
+
+    return parseInt(localStorage.getItem('puntajeTmp')) === 100;
+        
+}
---
+++ js/login.js
@@ -0,0 +1,33 @@
+const container = document.querySelector("#container");
+
+const divRow  = document.createElement("div");
+divRow.classList.add('divCenter');
+const txtSuperTitle  = document.createElement("h1");
+txtSuperTitle.textContent = "Bienvenido a Quien quiere ganar!!";
+const txtTitle  = document.createElement("span");
+txtTitle.textContent = "Nombre de usuario completo!!!";
+
+const inputUser  = document.createElement("input");
+inputUser.classList.add('form-control','inputIdex');
+inputUser.setAttribute("placeholder", "Ingrese su nombre completo");
+inputUser.textContent = '';
+
+const btnIngresar  = document.createElement("button");
+btnIngresar.classList.add('btn','btn-success','btnIndex');
+btnIngresar.textContent = "Ingresar";
+
+divRow.append(txtSuperTitle,txtTitle,inputUser,btnIngresar);
+
+container.append(divRow);
+
+
+btnIngresar.addEventListener('click',  (e) =>{
+    console.log(inputUser.value);
+    if(inputUser.value === ""){
+        alert('Por favor ingrese el nombre del jugador');
+    }else{
+        localStorage.setItem('user', inputUser.value);
+        window.location.href = "home.html";
+    }
+
+});
